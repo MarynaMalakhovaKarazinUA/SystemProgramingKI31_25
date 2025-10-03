44bfbdf9b1b94fe99d831da09858a9e629112d23
Anhelina Cherkashchenko <anhelina.cherkashchenko@student.karazin.ua>
2025-10-03 08:14:23 +0000

    add error and greatings

--- myfirstfile.c
+++ myfirstfile.c
@@ -4,10 +4,19 @@
  а батьківський читає з цієї позиції. Продемонструвати спільність дескриптора. */
 
 #include <stdio.h>
-
 int main() {
-    printf("Hello, World!\n");
+    pid_t childPid;
+    switch (childPid = fork()) {
+        case -1: /* fork() failed */
+            printf("Error"); /* Handle error */
+            break;
+
+        case 0: /* Child of successful fork() comes here */
+            printf("Hello, I am child!"); /* Perform actions specific to child */
+            break;
+
+        default: /* Parent comes here after successful fork() */
+            printf("Hello, I am super mama!"); /* Perform actions specific to parent */
+    }
     return 0;
 }
-
-
